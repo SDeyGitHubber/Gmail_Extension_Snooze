c742b62a14a675b37b01039e6af369c5249a9091
SDeyGitHubber <shouvik.dey21@gmail.com>
2023-02-03 00:25:33 +0530

    initial commit

---
+++ content.js
@@ -0,0 +1,31 @@
+// Load the InboxSDK library
+var sdk = require("inboxsdk");
+var gapi=Window.gapi;
+// Initialize the InboxSDK with your API key
+sdk.load("1.0", "AIzaSyCXfHPh2iY9i5WgO3ZIcUyGMMHFPmRZxcs").then(function(sdk) {
+  var selectedthreadview=sdk.Conversations.getSelectedthreadvi  
+  // Create a button in the Gmail interface to snooze mails
+  sdk.Toolbars.addToolbarButtonForApp({
+    title: "Snooze",
+    iconUrl: "icon.png",
+    onClick: function(event) {
+        var selectedEmail=sdk.Widgets.getSelectedthreadvi()[0];
+        var emailId=selectedEmail.getMessageID();
+        var snoozetime=new Date();
+        snoozetime.setHours(snoozetime.getHours()+1);
+
+        gapi.client.gmail.users.threads.modify({
+            userId:"me",
+            id: emailId,
+            addLabelIds:["SNOOZE"],
+            removeLabelIds:["INBOX"],
+            resources:{
+                addLabelIds:["SNOOZE"],
+                removeLabelIds:["INBOX"]
+            }
+        }).then(function(response){
+            console.log("Email has been snoozed successfully");
+        });
+    }
+  });
+});
